61ff4f65da0425d7064e624f738091f4bbdff7d6
Tarek Behairy <tbtarek@gmail.com>
2021-03-25 22:43:53 -0400

    added the packages to server.js

---
+++ server.js
@@ -0,0 +1,5 @@
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const figlet = require("figlet");
+const connection = require("./db.js");
+const { printTable } = require("console-table-printer");
