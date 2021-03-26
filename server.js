a8bbe4130cbbb728f1182ba988e7ffdf00f0a5a5
Tarek Behairy <tbtarek@gmail.com>
2021-03-25 22:44:38 -0400

    adjusted server file

--- server.js
+++ server.js
@@ -3,3 +3,31 @@ const chalk = require("chalk");
 const figlet = require("figlet");
 const connection = require("./db.js");
 const { printTable } = require("console-table-printer");
+
+const init = () => {
+  connection.connect((err) => {
+    if (err) throw err;
+
+    console.log(
+      chalk.redBright(
+        "===================================================================================================="
+      )
+    );
+
+    console.log(chalk.blueBright(figlet.textSync("Employee Tracker")));
+
+    console.log(
+      chalk.yellowBright(
+        "                                         Created By: Tarek Behairy"
+      )
+    );
+
+    console.log(
+      chalk.redBright(
+        "===================================================================================================="
+      )
+    );
+    //begins questioning
+    mainMenu();
+  });
+};
