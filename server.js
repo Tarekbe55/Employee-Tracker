5da4ad407055f072d0ac3f5664dbdca9b5ba71e5
Tarek Behairy <tbtarek@gmail.com>
2021-03-25 22:45:26 -0400

    added questions to the application

--- server.js
+++ server.js
@@ -27,7 +27,107 @@ const init = () => {
         "===================================================================================================="
       )
     );
-    //begins questioning
+
     mainMenu();
   });
+};
+
+const mainMenu = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "mainMenuChoice",
+        choices: [
+          "Add Department",
+          "Add Role",
+          "Add Employee",
+          "View Departments",
+          "View Roles",
+          "View Employees",
+          "Update Employee Role",
+          "Update Employee Managers",
+          "View Employees by Manager",
+          "View Total Utilized Budget by Department",
+          "Remove Employee",
+          "Remove Department",
+          "Remove Role",
+          "Exit",
+        ],
+      },
+    ])
+    .then((response) => {
+      const answer = response.mainMenuChoice;
+      console.log(answer);
+      switch (answer) {
+        case "Add Department":
+          addDepartment();
+          break;
+        case "Add Role":
+          addRoles();
+          break;
+        case "Add Employee":
+          addEmployees();
+          break;
+        case "View Departments":
+          viewDepartments();
+          break;
+        case "View Roles":
+          viewRoles();
+          break;
+        case "View Employees":
+          viewEmployees();
+          break;
+        case "Update Employee Role":
+          updateRoles();
+          break;
+        case "Update Employee Managers":
+          updateEmployeeMgr();
+          break;
+        case "View Employees by Manager":
+          viewEmployeeByMgr();
+          break;
+        case "View Total Utilized Budget by Department":
+          departmentBudget();
+          break;
+        case "Remove Employee":
+          removeEmployee();
+          break;
+        case "Remove Department":
+          removeDept();
+          break;
+        case "Remove Role":
+          removeRole();
+          break;
+        case "Exit":
+          connection.end();
+          break;
+      }
+    });
+};
+
+const addDepartment = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the name of the department you are adding?",
+        name: "addDepartment",
+      },
+    ])
+    .then((response) => {
+      connection.query(
+        "INSERT INTO department SET ?",
+        {
+          department_name: response.addDepartment,
+        },
+        (err) => {
+          if (err) throw err;
+          console.log("Your department was created successfully!");
+
+          mainMenu();
+        }
+      );
+    });
 };
